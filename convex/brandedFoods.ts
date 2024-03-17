d8e0bdc2dffc98029e63d9c807ab44f02602403d
mashiourcse <mashiour.cseiiuc@gmail.com>
2024-03-17 09:19:15 +0600

    added

--- convex/brandedFoods.ts
+++ convex/brandedFoods.ts
@@ -3,6 +3,6 @@ import { query } from "./_generated/server";
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("brandedFoods").take(5);
+    return await ctx.db.query("brandedFoods").take(15);
   },
 });
